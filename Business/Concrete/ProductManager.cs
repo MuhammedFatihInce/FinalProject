78f73e7bb08ab636184939ce19059c337bc45ab6
Codcu-entel <88192139+Codcu-entel@users.noreply.github.com>
2023-05-15 15:06:16 +0300

    15 Mayıs

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,8 @@
 using Business.BusinessAspect.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Cashing;
+using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Business;
@@ -31,6 +33,7 @@ namespace Business.Concrete
 
 		[SecuredOperation("product.add,admin")]
 		[ValidationAspect(typeof(ProductValidator))]
+		[CacheRemoveAspect("IProductService.Get")]
 		public IResult Add(Product product)
 		{
 
@@ -47,6 +50,7 @@ namespace Business.Concrete
 
 		}
 
+		[CacheAspect]
 		public IDataResult<List<Product>> GetAll()
 		{
 			if (DateTime.Now.Hour == 2)
@@ -62,6 +66,7 @@ namespace Business.Concrete
 			return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
 		}
 
+		[CacheAspect]
 		public IDataResult<Product> GetById(int productId)
 		{
 			return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
@@ -78,6 +83,7 @@ namespace Business.Concrete
 		}
 
 		[ValidationAspect(typeof(ProductValidator))]
+		[CacheRemoveAspect("IProductService.Get")]
 		public IResult Update(Product product)
 		{
 			throw new NotImplementedException();
@@ -111,6 +117,20 @@ namespace Business.Concrete
 				return new ErrorResult(Messages.CategoryLimitExceded);
 			}
 			return new SuccessResult();
+		}
+
+		[TransactionScopeAspect]
+		public IResult AddTransactionalTest(Product product)
+		{
+			Add(product);
+			if (product.UnitPrice < 10)
+			{
+				throw new Exception("");
+			}
+
+			Add(product);
+
+			return null;
 		}
 	}
 }
